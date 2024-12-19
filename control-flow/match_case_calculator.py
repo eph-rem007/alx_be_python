5b31d906f41f00a94ba999dad5c04d5df93bc76b
eph-rem007 <ephraimfekadu@gmail.com>
2024-12-19 21:03:33 +0300

    contro

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,15 @@
+num1=int(input("Enter the fist number:"))
+num2=int(input("Enter the second number:"))
+operation= input("Choose the operation (+, -, *, /): ")
+match operation:
+    case '+':
+        print("The result is",num1+num2)
+    case '-':
+        print("The result is",num1-num2)
+    case '*':
+        print("The result is",num1*num2)
+    case '/':
+        if num2!=0:
+            print("The result is",num1/num2)
+        else:
+            print("Cannot divide by zero")
